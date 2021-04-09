793d91ecb41364863f2eb363eac29127ca67e830
Luis Vinicius <luisvinicius0906@gmail.com>
2021-04-09 17:45:36 -0300

    Remove useless code

--- src/project.js
+++ src/project.js
@@ -58,13 +58,6 @@ const projectCreation = () => {
   });
 }
 
-const projectTags = document.querySelector('p')
-
-for(let pTags in projectTags) {
-  const delBtn = docum
-}
-
-
 module.exports = {
   displayProjects,
   projectCreation,
